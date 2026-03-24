bf734bf235d8da8c324abda48a60e791c7d95f3e
Manver Singh <manveersingh0501l@gmail.com>
2026-03-24 21:06:04 +0530

    Added 110A.cpp

---
+++ A section problem/110A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    int c =0;
+    while(n){
+        int r = n%10;
+        if(r==4 || r==7) c++;
+        n/=10;
+
+    }
+    if(c==4 || c==7) cout<<"YES";
+    else cout<<"NO";
+}
