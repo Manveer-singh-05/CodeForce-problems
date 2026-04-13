f380b35834124cff06159746948cbc542c7ca3d1
Manver Singh <manveersingh0501l@gmail.com>
2026-04-13 21:49:07 +0530

    added 1873A.cpp

---
+++ A section problem/1873A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        string m;
+        cin>>m;
+        if(m=="cab"){
+            cout<<"NO"<<endl;
+        }
+        else if(m=="bca"){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+        }
+    }
+}
