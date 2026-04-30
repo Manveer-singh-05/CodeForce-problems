0e03b38d524a435487e1120af3436ba4a2049bad
Manver Singh <manveersingh0501l@gmail.com>
2026-04-30 17:45:56 +0530

    added 1294A.cpp

---
+++ A section problem/1294A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,n;
+        cin>>a>>b>>c>>n;
+        int sum = a+b+c+n;
+        if(sum%3!=0){
+            cout<<"NO"<<endl;
+        }
+        else{
+            int x = sum/3;
+            if(x>=max(a,max(b,c))){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
+        }
+    }
+}
