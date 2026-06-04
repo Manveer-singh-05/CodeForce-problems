5a7e29529add6239790e8cb47a247f3da36614f7
Manver Singh <manveersingh0501l@gmail.com>
2026-06-04 18:59:36 +0530

    added 1624A.cpp

---
+++ A section problem/1624A.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        long long mn = 1e18, mx = 0;
+
+        for (int i = 0; i < n; i++) {
+            long long x;
+            cin >> x;
+            mn = min(mn, x);
+            mx = max(mx, x);
+        }
+
+        cout << mx - mn << '\n';
+    }
+
+    return 0;
+}
