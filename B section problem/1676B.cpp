f27ff508b8a8afd6a3c83e9c111fdbed15aac8d5
Manver Singh <manveersingh0501l@gmail.com>
2026-06-18 13:50:46 +0530

    added 1676B.cpp

---
+++ B section problem/1676B.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        int mn = 1e9;
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+            mn = min(mn, a[i]);
+        }
+
+        long long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            ans += (a[i] - mn);
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
