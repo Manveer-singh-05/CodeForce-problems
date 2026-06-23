8cb5e1f5f74436b8adb2f3bfd0bdf2953d01429d
Manver Singh <manveersingh0501l@gmail.com>
2026-06-23 22:22:29 +0530

    fix error

--- A section problem/1881A.cpp
+++ A section problem/1881A.cpp
@@ -1,34 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main() {
     int t;
     cin >> t;
-
+ 
     while (t--) {
         int n, m;
         cin >> n >> m;
-
+ 
         string x, s;
         cin >> x >> s;
-
+ 
         int ans = 0;
-
-        while (true) {
+        bool found = false;
+ 
+        // Maximum 6-7 doublings are enough since n*m <= 25
+        for (int i = 0; i <= 6; i++) {
             if (x.find(s) != string::npos) {
                 cout << ans << "\n";
+                found = true;
                 break;
             }
-
-            if (x.size() > 2 * m) {
-                cout << -1 << "\n";
-                break;
-            }
-
             x += x;
             ans++;
         }
+ 
+        if (!found)
+            cout << -1 << "\n";
     }
-
+ 
     return 0;
 }
