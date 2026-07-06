deed710ba7641c1f76b298562e39d56d74f95c1c
Manver Singh <manveersingh0501l@gmail.com>
2026-07-06 22:02:41 +0530

    Added 1716A.cpp

---
+++ A section problem/1716A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int m;
+        cin>>m;
+        if(m==1){
+            cout<<2<<endl;
+        }
+        else{
+            cout<<(m+2)/3<<endl;
+        }
+        
+    }
+}
