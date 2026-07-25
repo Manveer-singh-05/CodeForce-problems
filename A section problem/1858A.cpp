c4be3ffdfbe81bf594775464c9f89020e4b37dff
Manver Singh <manveersingh0501l@gmail.com>
2026-07-25 21:27:17 +0530

    added 1858A.cpp

---
+++ A section problem/1858A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long a, b, c;
+        cin >> a >> b >> c;
+
+        if (a > b)
+            cout << "First\n";
+        else if (a < b)
+            cout << "Second\n";
+        else {
+            if (c % 2)
+                cout << "First\n";
+            else
+                cout << "Second\n";
+        }
+    }
+
+    return 0;
+}
