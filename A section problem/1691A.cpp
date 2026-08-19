42b43d6cae4d185baaaa8ea88acbf789c81b29bc
Manver Singh <manveersingh0501l@gmail.com>
2026-08-19 21:25:53 +0530

    added 1691A.cpp

---
+++ A section problem/1691A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int odd = 0, even = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+
+            if (x % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        cout << min(odd, even) << endl;
+    }
+
+    return 0;
+}
