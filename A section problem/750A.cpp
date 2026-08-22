2088c7c52deba8453919b3e12652a3672fc779c1
Manver Singh <manveersingh0501l@gmail.com>
2026-08-22 19:11:34 +0530

    added 750A.cpp

---
+++ A section problem/750A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    int available = 240 - k;
+    int time = 0;
+    int ans = 0;
+
+    for (int i = 1; i <= n; i++) {
+        time += 5 * i;
+
+        if (time <= available)
+            ans++;
+        else
+            break;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
