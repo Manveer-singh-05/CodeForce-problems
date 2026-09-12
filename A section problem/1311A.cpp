716c0eca4a8d5a93ce96bbc0fc85d2ccab6e1640
Manver Singh <manveersingh0501l@gmail.com>
2026-09-12 22:56:52 +0530

    added 1311A.cpp

---
+++ A section problem/1311A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long a, b;
+        cin >> a >> b;
+
+        if (a == b)
+            cout << 0 << '\n';
+        else if (a < b)
+            cout << ((b - a) % 2 ? 1 : 2) << '\n';
+        else
+            cout << ((a - b) % 2 == 0 ? 1 : 2) << '\n';
+    }
+}
